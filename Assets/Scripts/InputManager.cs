42a0292d6bea6cc3846c37f5a386dcc3b106e5ab
Kim Jørgensen <kim.joergensen@outlook.com>
2018-10-07 00:12:55 +0200

    init

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+public class InputManager {
+    [Serializable]
+    public class PlayerInputs {
+        [Tooltip("User input for moving the player towards the current mouse position.")]
+        public KeyCode Move = KeyCode.Mouse1;
+        [Tooltip("User input to fire the currently selected spell.")]
+        public KeyCode Fire = KeyCode.Mouse0;
+        [Tooltip("User input to stop ongoing player actions.")]
+        public KeyCode Stop = KeyCode.Space;
+    }
+}

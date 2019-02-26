9223cf201053b73247dddd67edbb1c26b81d58eb
Kim Jørgensen <kim.joergensen@outlook.com>
2019-02-26 22:47:45 +0100

    added methods on actionbar to set and get spell by key code

--- Assets/_Scripts/Actionbar/ActionBar.cs
+++ Assets/_Scripts/Actionbar/ActionBar.cs
@@ -1,22 +1,44 @@
 using System.Collections.Generic;
 using UnityEngine;
-using WarclockBrawl;
 using WarlockBrawl.Spells.Interfaces;
 
 namespace WarlockBrawl.Actionbar {
-    public class ActionBar {
+    public static class ActionBar {
         #region Class variables
-        private Dictionary<KeyCode, ISpell> _actionBarMap;
+        private static Dictionary<KeyCode, ISpell> _actionBarDictionary;
         #endregion
 
-        public void SetSpell(KeyCode keyCode, ISpell spell) {
+        /// <summary>
+        /// Set an <see cref="ISpell"/> on the actionbar slot mapped to <paramref name="keyCode"/>.
+        /// </summary>
+        /// <param name="keyCode"><see cref="KeyCode"/> of the actionbar slot.</param>
+        /// <param name="spell"><see cref="ISpell"/> to map to the actionbar slot.</param>
+        public static void SetSpell(KeyCode keyCode, ISpell spell) {
+            // Check if the key code is already mapped in the dictionary.
+            if (_actionBarDictionary.ContainsKey(keyCode))
+                // Remove the current spell from the actionbar to make room for the new.
+                _actionBarDictionary.Remove(keyCode);
 
+            // Add the new spell to the key code.
+            _actionBarDictionary.Add(keyCode, spell);
         }
 
-        public ISpell TryGetSpell(KeyCode keyCode) {
-            return _actionBarMap.TryGetValue(keyCode, out var spell)
-                ? spell
-                : null;
+        /// <summary>
+        /// Get the <see cref="ISpell"/> mapped to <paramref name="keyCode"/>.
+        /// </summary>
+        /// <param name="keyCode">Actionbar <see cref="KeyCode"/> pressed.</param>
+        /// <param name="spell">Spell mapped to the action bar with the pressed <see cref="KeyCode"/></param>
+        /// <returns>True if a spell was found. False if no spell was found.</returns>
+        public static bool TryGetSpell(KeyCode keyCode, out ISpell spell) {
+            // Set default out value to null.
+            spell = null;
+
+            // Try to set the out value to the spell mapped on the passed key code.
+            // Returns false if no spell could be found.
+            if (!_actionBarDictionary.TryGetValue(keyCode, out spell)) return false;
+
+            // Return true if a spell is successfully return to the out value.
+            return true;
         }
     }
 }
